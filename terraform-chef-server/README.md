2ae5a7f1174cf576d40990f597b33ffc28f186e9
Nikita Dinavahi <nikita.dinavahi@reancloud.com>
2016-06-03 13:10:12 -0400

    Changes

--- terraform-chef-server/README.md
+++
@@ -1,2 +0,0 @@
-# terraform-chef-server
-Uses terraform to create a chef server on an AWS Account
--- terraform-chef-server/outputs.tf
+++
@@ -1,1 +0,0 @@
-output "chef_server_private_ip"  { value = "${module.chef-server.chef_server_private_ip}" }
